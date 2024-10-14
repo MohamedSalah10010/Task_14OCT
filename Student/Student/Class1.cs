bfb0dd7cf0589dac5ecde3dae5dbafc0e69e465f
MohamedSalah10010 <abosalah100001000@gmail.com>
2024-10-14 22:51:03 +0300

    Student Task

---
+++ Student/Student/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_class_library
+{
+    public class Student
+    {
+       
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public double Grade { get; set; }
+        public string Major { get; set; }
+
+        public Student(string firstName, string lastName, int age, double grade, string major)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Grade = grade;
+            Major = major;
+        }
+
+       
+
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        
+        public string DisplayDetails()
+        {
+            return $"Name: {GetFullName()}\nAge: {Age}\nMajor: {Major}\nGrade: {Grade}  \"{getGrade()}\" ";
+           
+        }
+
+        public char getGrade() {
+
+            if (Grade >= 85)
+                return 'A';
+            else if (Grade >= 75)
+                return 'B';
+            else if (Grade >= 65)
+                return 'C';
+            else if (Grade >= 55)
+                return 'D';
+            else
+                return 'F';
+
+        }
+    }
+}
---
+++ Test_student/Test_student/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Student_class_library;
+
+namespace Test_student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S1 = new Student("Mohamed", "Salah", 24,80,"Computer Engineering");
+
+            Student S2 = new Student("Ahmed", "Saleh", 21, 77, "Data Engineering");
+
+            Console.WriteLine(S1.DisplayDetails());
+            Console.WriteLine();
+            Console.WriteLine(S2.DisplayDetails());
+            Console.ReadKey();
+        }
+    }
+}
