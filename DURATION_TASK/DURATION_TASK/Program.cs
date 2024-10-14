2226f4353bdd91822b9129124707e0674e8685f8
MohamedSalah10010 <abosalah100001000@gmail.com>
2024-10-14 22:18:53 +0300

    Upload class library duration and test it

---
+++ DURATION_TASK/DURATION_TASK/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DURATION_TASK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Duration D1 = new Duration(1, 10, 15);
+            Console.WriteLine(D1.getString());
+            Duration D2 = new Duration(3600);
+            Console.WriteLine(D2.getString());
+            Duration D3 = new Duration(7800);
+            Console.WriteLine(D3.getString());
+            Duration D4 = new Duration(666);
+            Console.WriteLine(D4.getString());
+        }
+    }
+}
---
+++ Duration/Duration/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Duration
+{
+
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+
+    public Duration(int hours, int minutes, int seconds)
+    {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public Duration(int totalSeconds)
+    {
+
+        this.hours = totalSeconds / 3600;
+        totalSeconds %= 3600;
+        this.minutes = totalSeconds / 60;
+        this.seconds = totalSeconds % 60;
+    }
+
+
+    public string getString()
+    {
+        if (hours > 0)
+            return $"Hours:{hours}, Minutes: {minutes}, Seconds: {seconds}";
+        else
+            return $"Minutes: {minutes}, Seconds: {seconds}";
+
+    }
+
+
+}
